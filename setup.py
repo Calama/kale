435e82cbace47b87f7a38d2801423d1df0fa04a1
Philip Schleihauf <uniphil@gmail.com>
2013-02-26 15:20:01 -0500

    packaging

---
+++ setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+    kale setup script
+
+    blah blah blah
+
+    :copyright: 2013 Calama Consulting, written and maintained by uniphil
+    :license: :) see http://license.visualidiot.com/
+"""
+
+from setuptools import setup
+
+
+readme = open('README.md').read()
+
+
+setup(
+    name='kale',
+    version='0.0.1',
+    author='Philip Schleihauf',
+    author_email='uniphil@gmail.com',
+    license=':) released by Calama Consulting',
+    description='Mongodb model helpers',
+    long_description=readme,
+    install_requires=['pymongo', 'simplejson'],
+    py_modules=['kale'],
+)
